d0d710469c326ba48a43a2fae0819258c76f9b73
juliabush <j.elizabethbush@gmail.com>
2025-04-10 00:01:23 +0200

    Adding a calculator button witheffects and starting javscript

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const billValue = document.getElementById('billvalue');
 const resetButton = document.getElementById('reset');
+const calculateButton = document.getElementById('calculate')
 const tipButtonCustom = document.getElementById('tipcustom')
-const numberButtons = document.querySelectorAll('[data-number]')
+const numberButtons = document.querySelectorAll('.buttons')
 
-
+let userBillCalculation = fucntion(n) {
+    billValue.value % numberButtons.value
+}
 
 resetButton.addEventListener('click', function() {
     location.reload();
