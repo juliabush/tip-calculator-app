add3209b040309b55765f7bf2c341fcbc44be24d
juliabush <j.elizabethbush@gmail.com>
2025-04-27 21:16:36 +0200

    JS for calculator basic buttons is working, trying to clean up html and css

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
 const billInput = document.getElementById("billvalue");
 const peopleInput = document.getElementById("people");
-const tipInputButton = document.querySelectorAll(".button");
+const tipInputButtons = document.querySelectorAll(".button");
+const customTipInput = document.getElementById("tipcustom");
+
+// Result elements
+
+const totalAmount = document.querySelector(".result");
+const totalAmountPerPerson = document.querySelector(".total-result");
+
+// Function to update results
+function updateResults(tipAmount, totalAmountValue) {
+  totalAmount.textContent = `$${tipAmount.toFixed(2)}`;
+  totalAmountPerPerson.textContent = `$${totalAmountValue.toFixed(2)}`;
+}
+
+// calculate tip
+tipInputButtons.forEach((inputButton) => {
+  inputButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const tipPercentage =
+      parseFloat(inputButton.innerText.replace("&", "")) / 100;
+    amount = parseFloat(billInput.value);
+    people = peopleInput.value;
+
+    const tipAmount = (amount * tipPercentage) / people;
+    const totalAmountValue = amount / people + tipAmount;
+
+    updateResults(tipAmount, totalAmountValue);
+  });
+});
