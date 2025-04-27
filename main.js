7de22f530883f80e9b216beeb38255c3ea2780c6
juliabush <j.elizabethbush@gmail.com>
2025-04-27 20:25:43 +0200

    Starting the JS for the tip calculator, getting rid of button default behavior

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-const billValue = document.getElementById('billvalue');
-const resetButton = document.getElementById('reset');
-const calculateButton = document.getElementById('calculate')
-const tipButtonCustom = document.getElementById('tipcustom')
-const numberButtons = document.querySelectorAll('.buttons')
-
-let userBillCalculation = fucntion(n) {
-    billValue.value % numberButtons.value
-}
-
-resetButton.addEventListener('click', function() {
-    location.reload();
-})
+const billInput = document.getElementById("billvalue");
+const peopleInput = document.getElementById("people");
+const tipInputButton = document.querySelectorAll(".button");
